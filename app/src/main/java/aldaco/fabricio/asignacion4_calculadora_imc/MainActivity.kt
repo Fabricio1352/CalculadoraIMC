0e34f59b0b0f2225f5d936ac96c799a3cf63763a
fabricio <aldacoaguilerafabricio@gmail.com>
2025-02-12 22:36:14 -0500

    a

--- app/src/main/java/aldaco/fabricio/asignacion4_calculadora_imc/MainActivity.kt
+++ app/src/main/java/aldaco/fabricio/asignacion4_calculadora_imc/MainActivity.kt
@@ -2,56 +2,45 @@ package aldaco.fabricio.asignacion4_calculadora_imc
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import aldaco.fabricio.asignacion4_calculadora_imc.ui.theme.Asignacion4_calculadora_IMCTheme
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
 
-    // variables
-    var peso =0.0
-    var estatura = 0.0
-    var imc =0.0
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //evento clicl boton
+        val btnCosto: Button = findViewById(R.id.btnCosto)
+        val btnSedan: Button = findViewById(R.id.btnSedan)
+        val btnCamioneta: Button = findViewById(R.id.btnCamioneta)
+        val btnDeportivo: Button = findViewById(R.id.btnDeportivo)
+        val tvCostoPoliza: TextView = findViewById(R.id.tvCostoPoliza)
+        val tvTotal: TextView = findViewById(R.id.tvTotal)
+        var costoTotal = 0
 
-        val calcular: Button = findViewById(R.id.btnCalcular)
-        val peso2: TextView = findViewById(R.id.etPeso)
-        val estatura2: TextView = findViewById(R.id.etEstatura)
-        val tvImc: TextView = findViewById(R.id.tvImc)
+        btnSedan.setOnClickListener {
+            tvCostoPoliza.setText("Poliza $500")
+            costoTotal = 500
+        }
+        btnCamioneta.setOnClickListener {
+            tvCostoPoliza.setText("Poliza $700")
+            costoTotal = 700
+        }
+        btnDeportivo.setOnClickListener {
+            tvCostoPoliza.setText("Poliza $1200")
+            costoTotal = 1200
+        }
 
+        val etAnos: EditText = findViewById(R.id.etAnos)
 
-        calcular.setOnClickListener {
-            peso = peso2.text.toString().toDoubleOrNull() ?: 0.0
-            estatura = estatura2.text.toString().toDoubleOrNull() ?: 0.0
+        btnCosto.setOnClickListener {
+            val anos = etAnos.text.toString().toIntOrNull() ?: 1
 
-            imc = peso / estatura
-
-            tvImc.setText("IMC: "+ String.format("%.2f", imc))
-
+            costoTotal = costoTotal * anos
+            tvTotal.setText("Total: $" + costoTotal)
+            println("El costo es $costoTotal")
         }
     }
-
-
 }
-
-
-
-
